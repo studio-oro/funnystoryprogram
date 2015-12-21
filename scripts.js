b0d6ba8d4c2c024005f2149a37c9597b663e1b37
studio-oro <SHALEE@ENG-SLEE.ad.nsf.gov>
2015-12-21 12:40:46 -0500

    Basic javascript Program

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var noun = prompt("Give me a noun:");
+var adj = prompt("Name an adjective:");
+var verb = prompt("List a verb:");
+alert("You're all done! Read your funny story now:");
+var funnystory = 'One day I was walking and I stepped on a '+ noun + ". ";
+funnystory += "It looked like " + adj + " cat.";
+funnystory += " So I decided to " + verb + " and eat it!"
+document.write(funnystory);
